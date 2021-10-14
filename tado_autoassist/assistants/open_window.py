54188cb8ef6b6f34c9a72ab399e415b05e3ec80e
Tim Pietz <tim@pietz.me>
2021-10-14 14:46:32 +0200

    Implement logging for open window assistant

--- tado_autoassist/assistants/open_window.py
+++ tado_autoassist/assistants/open_window.py
@@ -1,12 +1,32 @@
+import logging
 from typing import Dict
+
+from PyTado.interface import Tado
 
 from tado_autoassist.assistants.assistant import Assistant
 
+logger = logging.getLogger(__name__)
+
 
 class OpenWindowAssistant(Assistant):
+    def __init__(self, tado: Tado):
+        super().__init__(tado=tado)
+        self._zones_cache: Dict[int, Dict] = {}
+
     def check(self):
         zone_states: Dict[int, Dict] = {int(id_): state for id_, state in self._tado.getZoneStates()['zoneStates'].items()}
 
-        for zone_id, zone in zone_states.items():
-            if zone.get('openWindowDetected', False):
+        for zone_id, zone_state in zone_states.items():
+            if zone_state.get('openWindowDetected', False):
                 self._tado.setOpenWindow(zone_id)
+
+                zone = self._get_zone(zone_id)
+                logger.info(f'Detected an open window in {zone["name"]}. Enabling open window mode.')
+
+    def _get_zone(self, zone_id: int) -> Dict:
+        """Get zone metadata (not state) from cache if available"""
+        if zone_id not in self._zones_cache:
+            self._zones_cache = {zone.id: zone for zone in self._tado.getZones()}
+
+        return self._zones_cache[zone_id]
+
