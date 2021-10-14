c8146f09c7138ddb12612963d426cca17a833801
Tim Pietz <tim@pietz.me>
2021-10-14 12:43:29 +0200

    Add retry strategy with exponential backoff for unattended use

--- tado_autoassist.py
+++ tado_autoassist.py
@@ -1,11 +1,20 @@
 import asyncio
 from typing import Dict
 
+import requests
+import urllib3
 from PyTado.interface import Tado
+from requests.adapters import HTTPAdapter
+
+BACKOFF_FACTOR = 1.0
 
 
 async def main():
-    tado = Tado(username='username', password='password')
+    tado = Tado(
+        username='username',
+        password='password',
+        http_session=create_http_session(),
+    )
     await OpenWindowAssistant(tado=tado).run(check_interval=10.0)
 
 
@@ -24,6 +33,19 @@ class OpenWindowAssistant:
         for zone_id, zone in zone_states.items():
             if zone.get('openWindowDetected', False):
                 self._tado.setOpenWindow(zone_id)
+
+
+def create_http_session() -> requests.Session:
+    # noinspection PyTypeChecker
+    retry_strategy = urllib3.Retry(
+        total=None,
+        backoff_factor=BACKOFF_FACTOR,
+    )
+    adapter = HTTPAdapter(max_retries=retry_strategy)
+    session = requests.Session()
+    session.mount('http://', adapter)
+    session.mount('https://', adapter)
+    return session
 
 
 if __name__ == '__main__':
