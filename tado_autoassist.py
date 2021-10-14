3f2f992653343940f0b7139546f04d8ef390a67f
Tim Pietz <tim@pietz.me>
2021-10-14 13:24:42 +0200

    Implement basic geofencing assistant

--- tado_autoassist.py
+++ tado_autoassist.py
@@ -15,10 +15,13 @@ async def main():
         password='password',
         http_session=create_http_session(),
     )
-    await OpenWindowAssistant(tado=tado).run(check_interval=10.0)
+    await asyncio.gather(
+        OpenWindowAssistant(tado=tado).run(check_interval=10.0),
+        GeofencingAssistant(tado=tado).run(check_interval=60.0),
+    )
 
 
-class OpenWindowAssistant:
+class Assistant:
     def __init__(self, tado: Tado):
         self._tado = tado
 
@@ -28,11 +31,27 @@ class OpenWindowAssistant:
             await asyncio.sleep(check_interval)
 
     def check(self):
+        raise NotImplementedError()
+
+
+class OpenWindowAssistant(Assistant):
+    def check(self):
         zone_states: Dict[int, Dict] = {int(id_): state for id_, state in self._tado.getZoneStates()['zoneStates'].items()}
 
         for zone_id, zone in zone_states.items():
             if zone.get('openWindowDetected', False):
                 self._tado.setOpenWindow(zone_id)
+
+
+class GeofencingAssistant(Assistant):
+    def check(self):
+        mobile_devices = self._tado.getMobileDevices()
+        is_someone_home = any(device['location']['atHome'] for device in mobile_devices)
+
+        if is_someone_home:
+            self._tado.setHome()
+        else:
+            self._tado.setAway()
 
 
 def create_http_session() -> requests.Session:
