6696f773f1ca80dbf55a6fcb93bba72468ba751e
Tim Pietz <tim@pietz.me>
2021-10-23 23:24:41 +0200

    Use logging level WARNING for external libraries, INFO for tado_autoassist

--- tado_autoassist/logging.py
+++ tado_autoassist/logging.py
@@ -9,8 +9,11 @@ def configure_logging(log_file_name: Optional[str | PathLike[str]] = None):
         handlers.append(logging.FileHandler(log_file_name))
 
     logging.basicConfig(
-        level=logging.INFO,
+        level=logging.WARNING,
         format='%(asctime)s.%(msecs)03d %(levelname)s %(module)s: %(message)s',
         datefmt='%Y-%m-%d %H:%M:%S',
         handlers=handlers,
     )
+
+    library_name = __name__.split('.')[0]
+    logging.getLogger(library_name).setLevel(logging.INFO)
